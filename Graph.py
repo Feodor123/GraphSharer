7f96ad8e7a106e78790b8c01a5e008214f9b9cf1
Feodor123 <fedor.a.kiselev@gmail.com>
2020-05-17 19:05:28 +0300

    Project done; only tests left

--- Graph.py
+++ Graph.py
@@ -1,8 +1,10 @@
 from random import randint
+from math import sin, cos, pi
 
 
 class Link:
     def __init__(self, n1, n2, w=0):
+        """строится по номерам нод"""
         self.n1 = n1
         self.n2 = n2
         self.w = w
@@ -10,6 +12,7 @@ class Link:
 
 class Node:
     def __init__(self, x, y, num):
+        """coordinates in range [-1;1] or it will be out of screen"""
         self.links = []
         self.num = num
         self.x = x
@@ -21,42 +24,44 @@ class Graph:
         self.nodes = []
         self.links = []
 
-    def from_text(self, text):
+    def from_text(self, text, positions=[]):
         self.nodes = []
         self.links = []
-        lines = text.split('\r\n')
+        lines = text.split('\n')
         lines = [ln for ln in lines if not ln.startswith('#') and ln != ""]
-        n, m = tuple(map(int, lines[0].split(' ')))
-        assert m == len(lines) - 1, \
-            "not enough lines: {}/{}".format(len(lines) - 1, m) if \
-            (m > len(lines) - 1)\
-            else "too many lines: {}/{}".format(len(lines) - 1, m)
+        n = tuple(map(int, lines[0].split(' ')))[0]
         for i in range(n):
-            self.nodes.append(Node(randint(0, 100), randint(0, 100), i + 1))
+            if i < len(positions):
+                self.nodes.append(Node(*positions[i], i))
+            else:
+                self.nodes.append(Node(0.8 * sin(pi*2*i/n),
+                                        0.8 * cos(pi*2*i/n), i))
         for i in range(1, len(lines)):
-            ints = tuple(map(int, lines[i].split(' ')))
-            assert len(ints) == 2 or len(ints) == 3
+            try:
+                ints = tuple(map(int, lines[i].split(' ')))
+                assert len(ints) == 2 or len(ints) == 3
+            except:
+                continue
             if len(ints) == 2:
                 i1, i2 = ints
                 w = 0
             else:
                 i1, i2, w = ints
-            link = Link(self.nodes[i1 - 1], self.nodes[i2 - 1], w)
+            link = Link(i1 - 1, i2 - 1, w)
             self.links.append(link)
             self.nodes[i1 - 1].links.append(link)
             self.nodes[i2 - 1].links.append(link)
 
     def to_dict(self):
-        return {"n": len(self.nodes),
-                "m": len(self.links),
-                **{"l{}i1".format(i): lk.n1.num for i, lk in enumerate(
-                    self.links)},
-                **{"l{}i2".format(i): lk.n2.num for i, lk in enumerate(
+        return {"n": len(self.nodes), "m": len(self.links),
+                        **{"l{}i1".format(i): lk.n1 for i, lk in enumerate(
                             self.links)},
-                **{"l{}w".format(i): lk.w for i, lk in enumerate(
+                        **{"l{}i2".format(i): lk.n2 for i, lk in enumerate(
                             self.links)},
-                **{"n{}x".format(i): nd.x for i, nd in enumerate(
+                        **{"l{}w".format(i): lk.w for i, lk in enumerate(
+                            self.links)},
+                        **{"n{}x".format(i): nd.x for i, nd in enumerate(
                             self.nodes)},
-                **{"n{}y".format(i): nd.y for i, nd in enumerate(
+                        **{"n{}y".format(i): nd.y for i, nd in enumerate(
                             self.nodes)},
-                }
+                        }
